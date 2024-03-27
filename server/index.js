faab21baaff2d97f4b1d06de3b4b25aaf4b42118
sonisoniya <35595102+sonisoniya@users.noreply.github.com>
2024-03-27 10:57:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,49 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
+const snowflake = require('snowflake-sdk');
+const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+const port = 3002;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+const connectionOptions = {
+    account: 'deb05201.us-east-1',
+    username: 'sebastian',
+    password: 'Sebastian@02',
+    warehouse: 'COMPUTE_WH',
+    database: 'SNOWFLAKE_SAMPLE_DATA',
+    schema: 'TPCDS_SF100TCL'
+};
+
+// Create the Snowflake connection
+const connection = snowflake.createConnection(connectionOptions);
+
+// Connect to Snowflake
+connection.connect((err, conn) => {
+    if (err) {
+        console.error('Error connecting to Snowflake:', err.message);
+        return;
+    }
+    console.log('Connected to Snowflake!');
+});
+
+// Define the endpoint
+app.get('/greet', (request, response) => {
+    const sqlQuery = 'select cc_call_center_id, cc_name from SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CALL_CENTER limit 5';
+    connection.execute({
+        sqlText: sqlQuery,
+        complete: (err, stmt, rows) => {
+            if (err) {
+                console.error('Error executing SQL query:', err.message);
+                response.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+            console.log('Query executed successfully');
+            response.json(rows);
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`API is running on port ${port}`);
 });
